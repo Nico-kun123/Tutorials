fc70bbf6a9cd50dc0a50dca7a9a95967209d7a1c
OwO <77405288+Nico-kun123@users.noreply.github.com>
2022-10-20 01:57:40 +0700

    Add files via upload

---
+++ Webpack/src/index.js
@@ -0,0 +1,8 @@
+// Для того чтобы достучаться до плагина
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// У самого Webpack уже есть встроенные плагины, их неплохо тоже импортировать
+const webpack = require('webpack');
+
+// Импортируем модуль для работ с путями
+const path = require('path');
